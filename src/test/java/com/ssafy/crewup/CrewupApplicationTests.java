adcb2f8cb0e0d7ccf2d32218e4b28c6e9e02d856
kimsungheon <tjdgjswkd@naver.com>
2025-12-05 17:16:20 +0900

    init

---
+++ src/test/java/com/ssafy/crewup/CrewupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.crewup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
