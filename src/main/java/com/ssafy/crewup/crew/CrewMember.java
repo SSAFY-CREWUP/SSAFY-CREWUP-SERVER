5f742032ac0cee0adc2f954f41554ae2b4c2d3c6
Willy <willy_1020@naver.com>
2025-12-16 15:38:01 +0900

    [✨ feat/#9]: 주요 도메인 엔티티(User/Crew/…/Board/Comment) 추가 - User, Crew, CrewMember, Course, CourseReview, CourseScrap, Schedule, ScheduleMember, Vote, VoteOption, VoteRecord, Board, Comment

---
+++ src/main/java/com/ssafy/crewup/crew/CrewMember.java
@@ -0,0 +1,26 @@
+package com.ssafy.crewup.crew;
+
+import com.ssafy.crewup.global.common.BaseTime;
+import com.ssafy.crewup.enums.CrewMemberRole;
+import com.ssafy.crewup.enums.CrewMemberStatus;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CrewMember extends BaseTime {
+    private Long id;              // crew_member.id
+    private Long crewId;          // FK -> crew.crew_id
+    private Long userId;          // FK -> users.user_id
+    private CrewMemberRole role;  // ENUM
+    private CrewMemberStatus status; // ENUM
+    private LocalDateTime appliedAt;
+    private LocalDateTime joinedAt;
+}
