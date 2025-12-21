44bd72c53f0fc3ca22887dd86e9ea79de910070e
Willy <willy_1020@naver.com>
2025-12-21 22:40:26 +0900

    [📂 file/#13]:  mapper폴더 인식을 위한 경로 수정

--- src/main/java/com/ssafy/crewup/CrewupApplication.java
+++ src/main/java/com/ssafy/crewup/CrewupApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.ssafy.crewup")
+@MapperScan("com.ssafy.crewup.**.mapper")
 public class CrewupApplication {
 
     public static void main(String[] args) {
