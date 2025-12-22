b34913eaf26fcf049ad688bf9d76dfef1e573ea7
Willy <willy_1020@naver.com>
2025-12-22 21:22:38 +0900

    [📂 feat/#15]: 크루 생성 서비스 메서드 구현 - createCrew 시그니처 추가 - 지역/활동시간 검증 후 Crew, CrewMember 저장 및 crewId 반환

--- src/main/java/com/ssafy/crewup/crew/service/impl/CrewServiceImpl.java
+++ src/main/java/com/ssafy/crewup/crew/service/impl/CrewServiceImpl.java
@@ -1,8 +1,59 @@
 package com.ssafy.crewup.crew.service.impl;
 
+import com.ssafy.crewup.crew.Crew;
+import com.ssafy.crewup.crew.CrewMember;
+import com.ssafy.crewup.crew.dto.request.CrewCreateRequest;
+import com.ssafy.crewup.crew.mapper.CrewMapper;
+import com.ssafy.crewup.crew.mapper.CrewMemberMapper;
 import com.ssafy.crewup.crew.service.CrewService;
+import com.ssafy.crewup.enums.CrewMemberRole;
+import com.ssafy.crewup.enums.CrewMemberStatus;
+import com.ssafy.crewup.enums.Region;
+import com.ssafy.crewup.global.common.code.ErrorCode;
+import com.ssafy.crewup.global.common.exception.CustomException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CrewServiceImpl implements CrewService {
+
+    private final CrewMapper crewMapper;
+    private final CrewMemberMapper crewMemberMapper;
+
+    @Override
+    public Long createCrew(CrewCreateRequest request, Long userId) {
+        if (!Region.isValidLabel(request.region())) {
+            throw new CustomException(ErrorCode.INVALID_REGION);
+        }
+
+        String time = request.activityTime();
+        if (!("오전".equals(time) || "오후".equals(time) || "저녁".equals(time) || "야간".equals(time))) {
+            throw new CustomException(ErrorCode.INVALID_ACTIVITY_TIME);
+        }
+
+        Crew crew = Crew.builder()
+            .name(request.name())
+            .region(request.region())
+            .description(request.description())
+            .crewImage(request.crewImage())
+            .memberCount(1)
+            .build();
+
+        crewMapper.insert(crew);
+
+        LocalDateTime now = LocalDateTime.now();
+        CrewMember leader = CrewMember.builder()
+            .crewId(crew.getId())
+            .userId(userId)
+            .role(CrewMemberRole.LEADER)
+            .status(CrewMemberStatus.ACCEPTED)
+            .appliedAt(now)
+            .joinedAt(now)
+            .build();
+        crewMemberMapper.insert(leader);
+
+        return crew.getId();
+    }
 }
